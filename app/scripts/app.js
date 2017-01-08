363181488ffc68742ccae8fe6bed0536353acdea
Andrey Karamnoff <karamnoff@gmail.com>
2017-01-08 19:37:21 +0300

    :arrow_up: Add svg4everybody

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,10 @@
 'use strict';
 
+import svg4everybody from 'svg4everybody';
+
 import '../blocks/responses/responses.js';
 import '../blocks/page-header/page-header.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+	svg4everybody();
+});
