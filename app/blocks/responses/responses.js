330a945efe1dfc5b4919d186c4cbdc62af1c7492
Andrey Karamnoff <karamnoff@gmail.com>
2017-01-11 13:05:50 +0300

    :bug: (blocks/responses) Update js

--- app/blocks/responses/responses.js
+++ app/blocks/responses/responses.js
@@ -6,10 +6,13 @@ export default window.addEventListener('DOMContentLoaded', () => {
 
 	const slider = document.querySelector('.js_slider');
 
-	lory(slider, {
-		slideSpeed: 600,
-		rewind: true,
-		rewindSpeed: 1200
-	});
+	if (slider) {
+
+		lory(slider, {
+			slideSpeed: 600,
+			rewind: true,
+			rewindSpeed: 1200
+		});
+	}
 
 });
