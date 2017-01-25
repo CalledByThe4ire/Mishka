480dc7a91cf919dabc268a707bf0afea6431097b
Andrey Karamnoff <karamnoff@gmail.com>
2017-01-25 22:17:20 +0300

    :art: (blocks/text-field/text-field.js) Format code

--- app/blocks/text-field/text-field.js
+++ app/blocks/text-field/text-field.js
@@ -1,11 +1,12 @@
 'use strict';
 
 export default window.addEventListener('DOMContentLoaded', () => {
+
 	const textField = Array.prototype.slice.call( document.querySelectorAll('.text-field .text-field__input') );
+	const email = document.querySelector('input[type=\'email\']');
+
 	const mqlMobile = window.matchMedia('only screen and (max-width: 767px)');
 	const mqlTabDesk = window.matchMedia('only screen and (min-width: 768px)');
-
-	const email = document.querySelector('input[type=\'email\']');
 
 	/**
 	 * Добавляет / убирает класс у элемента,
